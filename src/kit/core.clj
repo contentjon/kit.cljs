4d52e67528b8489441aa21b10c23cf1bcd98cdf8
Jan Krueger <jan.krueger@gmail.com>
2013-05-14 13:59:49 +0000

    Added shorthand macros for aset and aget

--- src/kit/core.clj
+++ src/kit/core.clj
@@ -1,5 +1,15 @@
 (ns kit.core)
 
+(defmacro ?
+  "A shorthand for aget"
+  [a & ks]
+  `(aget ~a ~@(map #(if (keyword? %) (name %) %) ks)))
+
+(defmacro !
+  "A shorthand for aset"
+  [a k v]
+  `(aset ~a ~(if (keyword? k) (name k) k) ~v))
+
 (defmacro undefined?
   "cljs.core undefined? is broken when no inlining is done.
    we define a new version here to work around that until
